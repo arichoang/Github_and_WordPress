a5bfcff8bdbb2f5a2efba676c61ce71978bc1f0a
arichoang <89424390+arichoang@users.noreply.github.com>
2025-09-23 12:07:19 -0400

    modifiying main

--- main.tf
+++ main.tf
@@ -23,33 +23,6 @@ resource "aws_instance" "my_server" {
   }
 }
 
-resource "aws_security_group" "allow_http_ssh" {
-  name = "allow_http"
-  description = "allow http" 
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "allow_http_ssh"
-  }
-}
-
 output "public_ip" {
   value = aws_instance.my_server.public_ip
 }
